444f40560ce6c277ba6a19e307942d516b13e172
Onifade Yemi Mubaraqat <114718950+Mubaraqat1@users.noreply.github.com>
2023-09-08 16:16:22 +0100

    Update stageone.js

--- stageone.js
+++ stageone.js
@@ -16,12 +16,10 @@ window.onload = function () {
   const current = document.querySelector('[data-testid="currentDayOfTheWeek"]');
   current.textContent = `The current day of the week is: ${getDayOfTheWeek()}`;
   const currentUTC = document.querySelector('[data-testid="currentUTCTime"]');
-  currentUTC.textContent = `The current UTC time is: ${hours} : ${minutes} : ${seconds}`;
+  currentUTC.textContent = `The current UTC time is: ${UTCTime} `;
 };
 
-const UTCTime = new Date();
-const hours = UTCTime.getUTCHours();
-const minutes = UTCTime.getUTCMinutes();
-const seconds = UTCTime.getUTCSeconds();
+const UTCTime = new Date().getTime;
+
 
 // window.onload = function () {};
